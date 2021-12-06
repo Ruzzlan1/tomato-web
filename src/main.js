5412c448f64c7382e973bf8570b12b345595a2e4
Ruzzlan <voramzoram1@gmail.com>
2021-12-07 01:19:41 +0400

    Adding observer to navbar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
-$('#timepicker').clockpicker({
-    placement: 'bottom',
-    autoclose: true
-});
+if ($('#timepicker').length) {
+    $('#timepicker').clockpicker({
+        placement: 'bottom',
+        autoclose: true
+    });
+}
 
-$('[data-toggle="datepicker"]').datepicker();
+if ($('[data-toggle="datepicker"]').length) {
+    $('[data-toggle="datepicker"]').datepicker();
+}
 
-$(':input[type=number]').on('mousewheel', function (e) {
-    e.preventDefault();
-});
+if ($(':input[type=number]').length) {
+    $(':input[type=number]').on('mousewheel', function (e) {
+        e.preventDefault();
+    });
+}
+
+// add Intersection observer api
+const header = document.querySelector('.page-header');
+const nav = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const opacityNav = function (entries) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) nav.classList.add('darker-bg');
+    else nav.classList.remove('darker-bg');
+};
+const obsOptions = {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+};
+
+const observer = new IntersectionObserver(opacityNav, obsOptions);
+observer.observe(header);
